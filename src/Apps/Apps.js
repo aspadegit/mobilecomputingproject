7fe302d9798436c5d864b40edcbe258bd2a1a8a2
mayajenk <92805893+mayajenk@users.noreply.github.com>
2024-04-16 16:55:57 -0400

    blockly setup on apps

--- src/Apps/Apps.js
+++ src/Apps/Apps.js
@@ -1,9 +1,49 @@
-import React from "react";
+// Apps.js
+import React, { useRef, useEffect } from 'react';
+import Blockly from 'blockly';
 
 function Apps() {
-    return (
-      <h1>Apps currently under construction</h1>
-    );
-  }
+  const workspaceRef = useRef(null);
+
+  useEffect(() => {
+    const toolboxXml = `
+      <xml id="toolbox" style="display: none">
+        <category name="Logic" colour="#5C81A6">
+          <block type="logic_compare"></block>
+          <block type="logic_operation"></block>
+          <block type="logic_negate"></block>
+        </category>
+        <category name="Loops" colour="#5CA65C">
+          <block type="controls_repeat_ext"></block>
+          <block type="controls_whileUntil"></block>
+        </category>
+        <category name="Math" colour="#5C68A6">
+          <block type="math_number"></block>
+          <block type="math_arithmetic"></block>
+        </category>
+        <category name="Text" colour="#5CA68D">
+          <block type="text"></block>
+          <block type="text_length"></block>
+        </category>
+        <category name="Variables" colour="#A65C81" custom="VARIABLE"></category>
+      </xml>
+    `;
   
-  export default Apps;
+    const workspace = Blockly.inject(workspaceRef.current, {
+      toolbox: toolboxXml,
+    });
+  
+    return () => {
+      workspace.dispose();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Apps currently under construction</h1>
+      <div ref={workspaceRef} style={{ height: '480px', width: '600px' }} />
+    </div>
+  );
+}
+
+export default Apps;
