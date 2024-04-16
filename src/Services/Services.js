0a549a33d471727e8cd18d2c0d4f5a9b2cf902f3
DeclanGazil02 <declan.gazil@ufl.edu>
2024-04-16 16:17:43 -0400

    boiler plate code in services

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React from "react"; 
 
 function Services() {
     return (
-      <h1>Services currently under construction</h1>
+      <div>
+        <div>
+          <h1>Services currently under construction</h1>
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
     );
   }
   
